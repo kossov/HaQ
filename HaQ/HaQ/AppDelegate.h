018fd4644837fa9bc0455d478eb4895f9ea9d959
Ognyan Kossov <ognyankossov@Ognqns-Mac.local>
2016-02-02 11:03:55 -0800

    Initial Project Commit

---
+++ HaQ/HaQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HaQ
+//
+//  Created by Ognyan Kossov on 2/2/16.
+//  Copyright © 2016 Ognyan Kossov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
